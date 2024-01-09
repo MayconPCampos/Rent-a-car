faf23b9e120c121d032ef5d64197dd9daa6ef746
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-09 12:41:25 -0300

    Created Icon component and stories

---
+++ src/components/atoms/Icon.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Icon = styled.div`
+  svg {
+    fill: #a0a0a0;
+    cursor: pointer;
+    transform: scale(2.5);
+    margin: 20px;
+  }
+
+  svg:hover {
+    fill: #191509;
+  }
+`;
+
+export default Icon;
