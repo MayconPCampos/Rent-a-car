f139f50bb5254949c5dbca427e22925f1c7096d4
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-10 00:42:42 -0300

    Created Hero with carousel

---
+++ src/components/molecules/Hero.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { SlideText } from "../atoms/Font";
+import styled from "styled-components";
+
+import Carousel from "react-bootstrap/Carousel";
+
+const Section = styled.div`
+  .carousel-caption {
+    text-align: left;
+    left: 5%;
+    bottom: 10%;
+  }
+`;
+
+const Hero = () => {
+  return (
+    <Section>
+      <Carousel fade controls={false} indicators={false}>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="./assets/slide01.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption className="carousel-caption">
+            <SlideText>
+              <p>
+                Exclusive & low cost<br></br> car rental
+              </p>
+            </SlideText>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="./assets/slide02.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption className="carousel-caption">
+            <SlideText>
+              <p>
+                Exclusive & low cost<br></br> car rental
+              </p>
+            </SlideText>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <img
+            className="d-block w-100"
+            src="./assets/slide03.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption className="carousel-caption">
+            <SlideText>
+              <p>
+                Exclusive & low cost<br></br> car rental
+              </p>
+            </SlideText>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Section>
+  );
+};
+
+export default Hero;
---
+++ src/components/molecules/Hero.stories.jsx
@@ -0,0 +1,8 @@
+import Hero from "./Hero";
+
+export default {
+  title: "Molecules/Hero",
+  component: Hero,
+};
+
+export const HeroStory = () => <Hero />;
