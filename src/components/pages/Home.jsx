3591bb11b332c51b25f8af65b8fe54145634bdfb
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-10 01:53:19 -0300

    Added Cards template to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import MenuBar from "../molecules/MenuBar";
 import Description from "../templates/Description";
 import Footer from "../templates/Footer";
 import Hero from "../molecules/Hero";
+import Cards from "../templates/Cards";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MenuBar />
       <Hero />
       <Description />
+      <Cards />
       <Footer />
     </>
   );
