6d7ce0b1a17a7f8b9693cf27a3fdd872f0de6982
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-10 01:54:19 -0300

    Created database.js file with cards data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const cards = [
+  {
+    title: "Premium car rental",
+    description: "Ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "./assets/card1.jpg",
+    link: "#",
+  },
+  {
+    title: "Sport car rental",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "./assets/card2.jpg",
+    link: "#",
+  },
+  {
+    title: "Limousine rental",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "./assets/card3.jpg",
+    link: "#",
+  },
+  {
+    title: "Safety & insurance",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "./assets/card4.jpg",
+    link: "#",
+  },
+  {
+    title: "How to book a car",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "./assets/card5.jpg",
+    link: "#",
+  },
+];
