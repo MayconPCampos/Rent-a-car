0f76dd11c9863d814b13bd7820414daa0d8fa39f
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-10 00:45:28 -0300

    Inserted grid and changed layout

--- src/components/templates/Description.jsx
+++ src/components/templates/Description.jsx
@@ -1,37 +1,62 @@
 import React from "react";
-import propTypes from "prop-types";
+import styled from "styled-components";
+import Grid from "@mui/material/Unstable_Grid2";
 
 import Text, { Bold, Title } from "../atoms/Font";
 import Button from "../atoms/Button";
 
+const Section = styled.div`
+  padding: 5%;
+
+  img {
+    width: 100%;
+    max-width: 300px;
+    height: auto;
+    margin: 10px;
+  }
+`;
+
+const Banner = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: auto;
+`;
+
 const Description = () => {
   return (
-    <>
-      <div>
-        <Title>
-          <h1>Welcome to exclusive car rental</h1>
-        </Title>
-        <Bold>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor
-          est lorem, eu tempor elit molestie tempus.
-        </Bold>
-        <Text>
-          Sed porttitor est lorem, eu tempor elit molestie tempus. Donec
-          molestie non arcu non aliquam. Fusce ante lectus, sagittis bibendum
-          turpis non, blandit luctus ex. In rhoncus tincidunt nisl, vel
-          consectetur augue vulputate eget. Nunc maximus turpis eget purus
-          pharetra, posuere lacinia purus gravida. Ut sagittis pharetra quam, ut
-          consequat mauris lobortis quis.
-        </Text>
-        <Button>
-          <a href="#">Read more</a>
-        </Button>
-      </div>
-      <div>
-        <img src="./assets/banner1.jpg" alt="" />
-        <img src="./assets/banner2.jpg" alt="" />
-      </div>
-    </>
+    <Section>
+      <Grid container>
+        <Grid xl={6} lg={12}>
+          <div>
+            <Title>
+              <h1>Welcome to exclusive car rental</h1>
+            </Title>
+            <Bold>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              porttitor est lorem, eu tempor elit molestie tempus.
+            </Bold>
+            <Text>
+              Sed porttitor est lorem, eu tempor elit molestie tempus. Donec
+              molestie non arcu non aliquam. Fusce ante lectus, sagittis
+              bibendum turpis non, blandit luctus ex. In rhoncus tincidunt nisl,
+              vel consectetur augue vulputate eget. Nunc maximus turpis eget
+              purus pharetra, posuere lacinia purus gravida. Ut sagittis
+              pharetra quam, ut consequat mauris lobortis quis.
+            </Text>
+            <Button>
+              <a href="#">Read more</a>
+            </Button>
+          </div>
+        </Grid>
+        <Grid container xl={6} lg={12}>
+          <Banner container>
+            <img src="./assets/banner1.jpg" alt="" />
+            <img src="./assets/banner2.jpg" alt="" />
+          </Banner>
+        </Grid>
+      </Grid>
+    </Section>
   );
 };
 
