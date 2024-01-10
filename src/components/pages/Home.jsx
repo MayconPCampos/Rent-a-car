cc72231acd459273c4d68dcf9f0f6c1ad887bfbb
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-10 00:44:27 -0300

    Created home page

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import MenuBar from "../molecules/MenuBar";
+import Description from "../templates/Description";
+import Footer from "../templates/Footer";
+import Hero from "../molecules/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <MenuBar />
+      <Hero />
+      <Description />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
