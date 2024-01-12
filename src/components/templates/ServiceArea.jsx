cb54483bcae606131f54350a2c78fad47f4fe87a
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-12 17:43:24 -0300

    New layout and styles

--- src/components/templates/ServiceArea.jsx
+++ src/components/templates/ServiceArea.jsx
@@ -5,18 +5,29 @@ import { Title } from "../atoms/Font";
 
 const Container = styled.div`
   display: flex;
-  width: 100%;
+  max-height: 650px;
+
+  @media screen and (max-width: 992px) {
+    flex-direction: column;
+    max-height: 100%;
+  }
 `;
 
 const States = styled.div`
   background-color: #141311;
   display: flex;
   flex-direction: column;
-  padding: 40px;
+  padding: 60px;
+  width: 100%;
+  max-height: 100%;
 
   a {
     color: #adaba6;
     text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
   }
 
   h2 {
@@ -24,27 +35,61 @@ const States = styled.div`
   }
 `;
 
+const StatesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-height: 100%;
+  padding-bottom: 40px;
+
+  a {
+    margin: 0 10px;
+  }
+
+  @media screen and (max-width: 992px) {
+    flex-direction: column;
+    max-height: 700px;
+  }
+`;
+
 const Services = styled.div`
   background-color: #2a2927;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px;
+
+  p {
+    color: #fff;
+  }
+
+  .service {
+    font-size: 1.6rem;
+    font-weight: bold;
+  }
 `;
 
 const ServiceArea = () => {
   return (
     <Container>
       <States>
-        <Title>
+        <Title id="service-area">
           <h2>Service Area</h2>
         </Title>
-        {states.map((states) => (
-          <a href="#">{states}</a>
-        ))}
+        <StatesList>
+          {states.map((states) => (
+            <a href="#service-area">{states}</a>
+          ))}
+        </StatesList>
       </States>
       <Services>
         {services.map((services) => (
           <div>
-            <div>{services.title}</div>
-            <div>{services.description}</div>
-            <div>{services.image}</div>
+            <p className="service">{services.title}</p>
+            <p>{services.description}</p>
+            <img src={services.image}></img>
           </div>
         ))}
       </Services>
