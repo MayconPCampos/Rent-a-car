ac3c280894108e12bcd9b9b24465375c580aa10c
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-12 15:32:24 -0300

    Added route using react routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Home from "../src/components/pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import GlobalStyles from "./GlobalStyles";
+
+import routes from "./routes/router";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Home />
+      <RouterProvider router={routes} />
     </>
   );
 }
